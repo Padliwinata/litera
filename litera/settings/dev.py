6f245f8993f8fb16265b01ff87e5d5b17c943379
Padliwinata <rpadliwinata@gmail.com>
2024-04-24 17:40:30 +0700

    update

--- litera/settings/dev.py
+++ litera/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-7xcq)&2yihw(-pszoj(1bp2qjyg7$-mi3bw%36q(he!vq#7i#b"
